3a44c23eeff2bba5ba945abedaea4df27f2ed499
Tim <timburnsowlmtn@gmail.com>
2026-03-17 08:47:58 -0400

    databricks study

--- notebooks/01_delta_lake/identity_spine.sql
+++ notebooks/01_delta_lake/identity_spine.sql
@@ -1,2 +1,6 @@
-CREATE TABLE tim_dev.cleanroom.identity_spine
+CREATE or replace TABLE tim_dev.cleanroom.identity_spine (
+  hashed_email STRING,
+  provider_id STRING,
+  last_seen TIMESTAMP
+)
 USING DELTA;
